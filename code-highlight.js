bd9c5f17314843a7376605953d3078a5d53a164e
rakitind00 <83398802+rakitind00@users.noreply.github.com>
2021-09-20 16:26:11 +0300

    Update code-highlight.js

--- code-highlight.js
+++ code-highlight.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
   var code = $(".codemirror-textarea")[0];
   var editor = CodeMirror.fromTextArea(code, {
-    lineNumbers : true
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: "text/x-csrc"
   });
 });
   
