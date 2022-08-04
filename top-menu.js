62627753ca2a080a22826b77ac203c9f38cf1382
rakitind00 <83398802+rakitind00@users.noreply.github.com>
2022-08-04 22:00:36 +0300

    Create top-menu.js

---
+++ top-menu.js
@@ -0,0 +1,3 @@
+ var elemDiv = document.createElement('div');
+ elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
+ document.body.appendChild(elemDiv);
